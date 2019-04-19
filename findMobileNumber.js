690bd3d15a78ddc046e6cd21d169eee1e4363d67
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2019-04-19 15:57:50 +0530

    finding multiple numbers

--- findMobileNumber.js
+++ findMobileNumber.js
@@ -18,18 +18,28 @@ function findMobileNumber(input) {
         zero: 0
     };
 
-    let digitsFound = [];
-
-    const isNumber = (n) => !isNaN(Number(n));
-
-    let tempDigit = "";
-
-    const isEmpty = (str) => typeof str === 'string' && str.length === 0;
+    const isEmpty = (inp) => Array.isArray(inp) || typeof inp === 'string' && inp.length === 0;
 
     const isNonAlphaNumeric = (ch) => {
         const code = ch.charCodeAt(0);
         return !((code > 64 && code < 91) || (code > 96 && code < 123) || (code > 47 && code < 58));
     }
+
+    const isNumber = (n) => !isNaN(Number(n));
+
+    const getValidNumber = (inp) => {
+        let index = 0;
+        while(index < inp.length && !numMap.hasOwnProperty(inp.slice(index))){
+            index++;
+        }
+        return inp.slice(index);
+    }
+
+    let digitsFound = [];
+
+    let tempDigit = "";
+
+    const output = [];
 
     input.split("").forEach(ch => {
         if(isNonAlphaNumeric(ch)){
@@ -40,20 +50,37 @@ function findMobileNumber(input) {
                 digitsFound.push(ch);
             }else{
                 digitsFound = [ch];
+                tempDigit = "";
             }
         }else{
             tempDigit += ch;
-            if(numMap.hasOwnProperty(tempDigit)){
-                digitsFound.push(numMap[tempDigit]);
+            const validNumber = getValidNumber(tempDigit);
+            if(!isEmpty(validNumber)){
+                if(validNumber.length < tempDigit.length){
+                    digitsFound = [numMap[validNumber]];
+                }else{
+                    digitsFound.push(numMap[validNumber]);
+                }
                 tempDigit = "";
             }
         }
+        //console.log(digitsFound);
+        if(digitsFound.length === 10){
+            output.push(digitsFound.join(""));
+            digitsFound = [];
+        }
     });
 
-    if(digitsFound.length % 10 === 0){
-        return digitsFound.join("");
-    }else{
-        return "No Number Found!";
-    }
+    return output;
 
 }
+
+const testCases = [
+    "9 five3threeone_6-two 0.0#six",
+    "90ui five3threeone_6-two9 five3threeone_6-two9 five3threeone_6-two9 five3threeone_6-two9 five3threeone_6-two9 five3threeklpone_6-two9 five3threeone_6-two9 five3threeonejkl_6-two9 five3threeone_6-two9 five3threeone_6-two 0.0#six",
+    "95331six200x9533onesix200six"
+]
+
+testCases.forEach(testCase => {
+    console.log(findMobileNumber(testCase));
+});
